b9ef0e3e4db61f559cc0f92a54d46726b89763ae
dishangti <16698219+dishangti@users.noreply.github.com>
2023-06-23 23:04:19 +0800

    Vector space of tabloid and product of Ct.

--- sprecht.py
+++ sprecht.py
@@ -43,6 +43,19 @@ class Tabloid:
             cnt += i
         return s[:-1]
 
+class CTabloid:
+    def __init__(self):
+        self.vect = []
+        self.sgn = []
+
+    def add(self, vect:Tabloid, sgn:int):
+        self.vect.append(vect)
+        self.sgn.append(sgn)
+
+    def permut(self, p):
+        for i in range(len(self.vect)):
+            self.vect[i] = self.vect[i].permut(p)
+
 def partition(part_list, n, last, part):
     """
     Get the partition of integer n
@@ -56,11 +69,39 @@ def partition(part_list, n, last, part):
         new_part.append(i)
         partition(part_list, n - i, i, new_part)
 
-def Ct(shape, p):
+def inversion(inv, p, t, m, n):
+    if (m == n): return
+    mid = (m+n)>>1
+
+    inversion(m, mid)
+    inversion(mid + 1, n)
+
+    i = m; j = mid + 1; k = m
+    while (i <= mid and j <= n):
+        if (a[i] <= a[j]):
+            t[k] = a[i]
+            k += 1; i += 1
+        else:
+            t[k] = a[j]
+            k += 1; j += 1
+            inv[0] += mid - i + 1
+
+    while (i <= mid):
+        t[k] = a[i]
+        k += 1; i += 1
+    while (j <= n):
+        t[k] = a[j]
+        k += 1; j += 1
+    for i in range(m, n+1):
+        a[i] = t[i] 
+
+def Ct(t):
     """
     Column stablizer
     """
-    Ct = []
+    shape, p = t.shape, t.p
+    col_Ct = []
+
     col = []
     n = len(p)
 
@@ -74,9 +115,17 @@ def Ct(shape, p):
         cnt += i
     
     for i in range(len(col)):
-        Ct.append(itertools.permutations(col[i]))
-    return Ct
+        col_Ct.append(list(itertools.permutations(col[i])))
 
+    if len(col) > 1:
+        Ct = list(itertools.product(col_Ct[0], col_Ct[1]))
+        for i in range(2, len(col)):
+            Ct = list(itertools.product(Ct, col_Ct[i]))
+            for j in range(len(Ct)):
+                Ct[j] = Ct[j][0] + Ct[j][1:]
+    else:
+        Ct = col_Ct[0]
+    return Ct
 
 if __name__ == "__main__":
     n = int(input())
@@ -101,5 +150,6 @@ if __name__ == "__main__":
     p = [2, 3, 1, 5, 4]
     print("Action on T1:")
     print(a.permut(p), "\n")
-
-    Ct([3, 2], [1,4,5,2,3])
+    
+    print('Column stablizer of T1')
+    print(Ct(Tabloid([3, 2], [1,4,5,2,3])))
