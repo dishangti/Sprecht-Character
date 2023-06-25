15a4ca1452efcdfeaa25deebe32014abe819b852
dishangti <16698219+dishangti@users.noreply.github.com>
2023-06-25 16:25:47 +0800

    Polyoid and basis of rep space basis.

--- sprecht.py
+++ sprecht.py
@@ -1,4 +1,6 @@
 import itertools
+import numpy as np
+import sympy
 
 class Tabloid:
     def sort_row(self):
@@ -8,6 +10,18 @@ class Tabloid:
             row.sort()
             self.p[cnt:cnt+i] = row
             cnt += i
+
+    def permut_Ct(self, p, base):
+        """
+        Action of column stablizer
+        """
+        new_t = Tabloid(self.shape, self.p)
+        for col in range(len(p)):
+            for row in range(len(p[col])):
+                b = new_t.p.index(base[col][row])
+                new_t.p[b] = p[col][row]
+        new_t.sort_row()
+        return new_t()
 
     def permut(self, p):
         """
@@ -73,27 +87,27 @@ def inversion(inv, p, t, m, n):
     if (m == n): return
     mid = (m+n)>>1
 
-    inversion(m, mid)
-    inversion(mid + 1, n)
+    inversion(inv, p, t, m, mid)
+    inversion(inv, p, t, mid + 1, n)
 
     i = m; j = mid + 1; k = m
     while (i <= mid and j <= n):
-        if (a[i] <= a[j]):
-            t[k] = a[i]
+        if (p[i] <= p[j]):
+            t[k] = p[i]
             k += 1; i += 1
         else:
-            t[k] = a[j]
+            t[k] = p[j]
             k += 1; j += 1
             inv[0] += mid - i + 1
 
     while (i <= mid):
-        t[k] = a[i]
+        t[k] = p[i]
         k += 1; i += 1
     while (j <= n):
-        t[k] = a[j]
+        t[k] = p[j]
         k += 1; j += 1
     for i in range(m, n+1):
-        a[i] = t[i] 
+        p[i] = t[i] 
 
 def Ct(t):
     """
@@ -125,7 +139,64 @@ def Ct(t):
                 Ct[j] = Ct[j][0] + Ct[j][1:]
     else:
         Ct = col_Ct[0]
+    Ct.sort()
     return Ct
+
+def sgn_Ct(p, n):
+    sgn = 1
+    t = [0] * n
+    print('3',p)
+    for i in range(len(p)):
+        inv = [0]
+        inversion(inv, list(p[i]), t, 0, len(p[i])-1)
+        inv = inv[0]
+        subsgn = (-((inv & 1) << 1)) + 1
+        sgn *= subsgn
+        print(p[i], subsgn, inv)
+    return sgn
+
+def get_et(shape, p, Ct):
+    """
+    Polyoid
+    """
+    et = CTabloid()
+    base = Ct[0]
+    for g in Ct:
+        sgn = sgn_Ct(g)
+        tb = Tabloid(shape, p)
+        tb = tb.permut_Ct(g, base)
+        et.add(tb, sgn)
+
+def get_basis(et, n):
+    permut = itertools.permutations(list(range(1, n+1)))
+    vects = []
+    assign = {}
+    inv_assign = {}
+    n = -1
+    for g in permut:
+        g_et = et.permut(g)
+        vects.append(g_et)
+
+        for vect in g_et.vect:
+            vect = tuple(vect)
+            if vect not in assign:
+                n += 1
+                assign[vect] = n
+                inv_assign[n] = vect
+
+    mat = np.zeros((n,1))
+    for v in vects:
+        v_cor = np.zeros((n, 1))
+        for tb in range(len(v.vect)):
+            v_cor[assign[v.vect[tb]]] = v.sgn(tb)
+        mat = np.c_[mat, v_cor]
+    
+    mat = mat[:, 1:]
+    _, basis_idx =  sympy.Matrix(mat).T.rref()
+    basis = []
+    for idx in basis_idx:
+        basis.append([vects[idx]])
+    return basis
 
 if __name__ == "__main__":
     n = int(input())
@@ -152,4 +223,6 @@ if __name__ == "__main__":
     print(a.permut(p), "\n")
     
     print('Column stablizer of T1')
-    print(Ct(Tabloid([3, 2], [1,4,5,2,3])))
+    ct = Ct(Tabloid([3, 2], [1,4,5,2,3]))
+    print(ct)
+    print(sgn_Ct(ct[1], 5))
